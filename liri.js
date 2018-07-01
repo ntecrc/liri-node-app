08e6902bf01a4aebe2e97f2ac9aa9760f426453c
ntecrc <gonzalezrc@gmail.ccom>
2018-07-01 10:21:41 -0400

    updated tweets

--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
 //NPM package for reading and writing files
 require("dotenv").config();
 
+// NPM package for reading and writing files
+var fs = require("fs");
+
 // Code required to import the `keys.js` file 
 // and store it in a variable
 var keys = require("./keys.js");
-
-// NPM package for reading and writing files
-var fs = require("fs");
 
 //NPM package for making ajax-like calls
 var request = require("request");				
@@ -15,45 +15,28 @@ var request = require("request");
 // var spotify = new Spotify(keys.spotify);
 
 
-// My-tweets This will show your last 20 tweets w
+// My-tweets This will show your last 20 tweets 
 // and when they were created at in your terminal/bash windo
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 
+var input = process.argv[2];
 
-function fetchTwitter(){
-    var tweetsLength;
-    var params = { screen_name: 'dev62437207' };
-    client.get('statuses/user_timeline', function (error, tweets, response) {
-        if (error) throw error;
-
-        tweetsLength = 0;
-
-        for(var i=0; i<tweets.length; i++){
-			tweetsLength ++;
-		}
-		if (tweetsLength > 20){
-			tweetsLength = 20;
-		}
-		for (var i=0; i< tweetsLength; i++){
-			console.log("Tweet " + (i+1) + " created on: " + tweets[i].created_at);
-			console.log("Tweet " + (i+1) + " text: " + tweets[i].text);
-			console.log("--------------------------------------------------------------");
-
-			// appendFile("Tweet " + (i+1) + " created on: " + tweets[i].created_at);
-			// appendFile("Tweet " + (i+1) + " text: " + tweets[i].text);
-			// appendFile("--------------------------------------------------------------");
-		}
-
-    });
+switch (input) {
+	case 'myTweet':
+		myTweets();
+	break;
 }
-
-
-
-
-
-
-
+	function myTweets(){
+			var params = { screen_name: 'dev62437207' };
+			client.get('statuses/user_timeline', params, function (error, tweets, response) {
+				if (!error) {
+					console.log(tweets.created_at);
+					console.log(tweets.text);
+				}
+			});
+	
+		}
 
 // request("http://www.omdbapi.com/?i=tt3896198&apikey=e49dacd7", function(error, response, body) {
 //     if (!error && response.statusCode === 200) {
